e35661fda16e79e20048d7b6e0833c72c9f25159
VENGELHARD <v.engelhard@lighthouse-mcs.de>
2022-06-23 15:44:14 +0200

    20220623

--- configs.js
+++ configs.js
@@ -2,6 +2,6 @@
 
 module.exports = 
 {
-    apiKey: '', // Enter Your API Key as server default
+    apiKey: '6bec40cf957de430a6f1f2baa056b99a4fac9ea0', // Enter Your API Key as server default
     apiUrl: 'https://api.meraki.com/api/v1'
 }
